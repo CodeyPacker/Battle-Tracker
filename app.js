ec16a86444019faaf9c0c0bba2f59f7a162ee342
CodeyPacker <codeypacker@gmail.com>
2018-07-28 23:11:49 -0600

    First Commit 🔥

---
+++ app.js
@@ -0,0 +1,393 @@
+var cards = {
+  abomination: {
+    level: 14
+  },
+  absorbing: {
+    level: 2,
+    imagePath: "../images/marvel/absorbingman.png"
+  },
+  aim: {
+    level: 1,
+    imagePath: "../images/marvel/aimsoldier.png"
+  },
+  arnimzola: {
+    level: 4,
+    imagePath: "../images/marvel/arnimzola.png"
+  },
+  baronvonstrucker: {
+    level: 12,
+    imagePath: "../images/marvel/baronvonstrucker.png"
+  },
+  baronzemo: {
+    level: 16,
+    imagePath: "../images/marvel/baronzemo.png"
+  },
+  bullseye: {
+    level: 2,
+    imagePath: "../images/marvel/bullseye.png"
+  },
+  carnage: {
+    level: 12,
+    imagePath: "../images/marvel/carnage.png"
+  },
+  crossbones: {
+    level: 4,
+    imagePath: "../images/marvel/crossbones.png"
+  },
+  doctor: {
+    level: 8,
+    imagePath: "../images/marvel/doctoroctopus.png"
+  },
+  electro: {
+    level: 6,
+    imagePath: "../images/marvel/electro.png"
+  },
+  greengoblin: {
+    level: 12,
+    imagePath: "../images/marvel/greengoblin.png"
+  },
+  hob: {
+    level: 6,
+    imagePath: "../images/marvel/hobgoblin.png"
+  },
+  hydra: {
+    level: 1,
+    imagePath: "../images/marvel/hydrasoldier.png"
+  },
+  kang: {
+    level: 16,
+    imagePath: "../images/marvel/kang.png"
+  },
+  kingpin: {
+    level: 12,
+    imagePath: "../images/marvel/kingpin.png"
+  },
+  klaw: {
+    level: 6,
+    imagePath: "../images/marvel/klaw.png"
+  },
+  kraven: {
+    level: 4,
+    imagePath: "../images/marvel/kraventhehunter.png"
+  },
+  lizard: {
+    level: 1,
+    imagePath: "../images/marvel/lizard.png"
+  },
+  loki: {
+    level: 20,
+    imagePath: "../images/marvel/loki.png"
+  },
+  madame: {
+    level: 8,
+    imagePath: "../images/marvel/madamehydra.png"
+  },
+  malekith: {
+    level: 16,
+    imagePath: "../images/marvel/malekith.png"
+  },
+  mandarin: {
+    level: 10,
+    imagePath: "../images/marvel/mandarin.png"
+  },
+  maximus: {
+    level: 10,
+    imagePath: "../images/marvel/maximus.png"
+  },
+  modok: {
+    level: 14,
+    imagePath: "../images/marvel/modok.png"
+  },
+  mysterio: {
+    level: 8,
+    imagePath: "../images/marvel/mysterio.png"
+  },
+  norman: {
+    level: 14,
+    imagePath: "../images/marvel/normanosborn.png"
+  },
+  redskull: {
+    level: 18,
+    imagePath: "../images/marvel/redskull.png"
+  },
+  rhino: {
+    level: 6,
+    imagePath: "../images/marvel/rhino.png"
+  },
+  sandman: {
+    level: 2,
+    imagePath: "../images/marvel/sandman.png"
+  },
+  scorpion: {
+    level: 1,
+    imagePath: "../images/marvel/scorpion.png"
+  },
+  shocker: {
+    level: 2,
+    imagePath: "../images/marvel/shocker.png"
+  },
+  super: {
+    level: 6,
+    imagePath: "../images/marvel/superadaptoid.png"
+  },
+  taskmaster: {
+    level: 8,
+    imagePath: "../images/marvel/taskmaster.png"
+  },
+  leader: {
+    level: 4,
+    imagePath: "../images/marvel/theleader.png"
+  },
+  unspoken: {
+    level: 8,
+    imagePath: "../images/marvel/theunspoken.png"
+  },
+  ultron: {
+    level: 18,
+    imagePath: "../images/marvel/ultron.png"
+  },
+  venom: {
+    level: 10,
+    imagePath: "../images/marvel/venom.png"
+  },
+  vulture: {
+    level: 1,
+    imagePath: "../images/marvel/vulture.png"
+  },
+  winter: {
+    level: 10,
+    imagePath: "../images/marvel/wintersoldier.png"
+  },
+  yellow: {
+    level: 10,
+    imagePath: "../images/marvel/yellowjacket.png"
+  },
+  //WIZARD OF OZ
+  black: {
+    level: 2,
+    imagePath: "../images/oz/blackbees.png"
+  },
+  blinkie: {
+    level: 6,
+    imagePath: "../images/oz/blinkie.png"
+  },
+  crows: {
+    level: 1,
+    imagePath: "../images/oz/crows.png"
+  },
+  fighting: {
+    level: 10,
+    imagePath: "../images/oz/fightingtrees.png"
+  },
+  flutter: {
+    level: 1,
+    imagePath: "../images/oz/flutterbudgets.png"
+  },
+  generalguph: {
+    level: 11,
+    imagePath: "../images/oz/generalguph.png"
+  },
+  generaljinjur: {
+    level: 16,
+    imagePath: "../images/oz/generaljinjur.png"
+  },
+  gurgles: {
+    level: 10,
+    imagePath: "../images/oz/gurgles.png"
+  },
+  gwig: {
+    level: 7,
+    imagePath: "../images/oz/gwig.png"
+  },
+  hammer: {
+    level: 3,
+    imagePath: "../images/oz/hammerheads.png"
+  },
+  hoppers: {
+    level: 9,
+    imagePath: "../images/oz/hoppers.png"
+  },
+  horners: {
+    level: 9,
+    imagePath: "../images/oz/horners.png"
+  },
+  invisible: {
+    level: 5,
+    imagePath: "../images/oz/invisiblebears.png"
+  },
+  irongiant: {
+    level: 13,
+    imagePath: "../images/oz/irongiant.png"
+  },
+  jackdaws: {
+    level: 1,
+    imagePath: "../images/oz/jackdaws.png"
+  },
+  kalidah: {
+    level: 15,
+    imagePath: "../images/oz/kalidah.png"
+  },
+  kingdox: {
+    level: 2,
+    imagePath: "../images/oz/kingdox.png"
+  },
+  kingkleaverofutensia: {
+    level: 5,
+    imagePath: "../images/oz/kingkleaverofutensia.png"
+  },
+  kingkrewlofjinxland: {
+    level: 17,
+    imagePath: "../images/oz/kingkrewlofjinxland.png"
+  },
+  limoneag: {
+    level: 12,
+    imagePath: "../images/oz/limoneag.png"
+  },
+  mombi: {
+    level: 11,
+    imagePath: "../images/oz/mombi.png"
+  },
+  nome: {
+    level: 3,
+    imagePath: "../images/oz/nome.png"
+  },
+  oztrich: {
+    level: 3,
+    imagePath: "../images/oz/oztrich.png"
+  },
+  phanfasms: {
+    level: 14,
+    imagePath: "../images/oz/phanfasms.png"
+  },
+  quox: {
+    level: 18,
+    imagePath: "../images/oz/quox.png"
+  },
+  rigmaroles: {
+    level: 1,
+    imagePath: "../images/oz/rigmaroles.png"
+  },
+  royal: {
+    level: 4,
+    imagePath: "../images/oz/royalgardeneroftherosekingdom.png"
+  },
+  scoodlers: {
+    level: 2,
+    imagePath: "../images/oz/scoodlers.png"
+  },
+  nome: {
+    level: 20,
+    imagePath: "../images/oz/thenomeking.png"
+  },
+  yoop: {
+    level: 13,
+    imagePath: "../images/oz/theyoop.png"
+  },
+  tititi: {
+    level: 12,
+    imagePath: "../images/oz/tititihoochoothegreatjinjin.png"
+  },
+  wheelers: {
+    level: 8,
+    imagePath: "../images/oz/wheelers.png"
+  },
+  whimsies: {
+    level: 7,
+    imagePath: "../images/oz/whimsies.png"
+  },
+  wickedwitchoftheeast: {
+    level: 8,
+    imagePath: "../images/oz/wickedwitchoftheeast.png"
+  },
+  wickedwitchofthewest: {
+    level: 19,
+    imagePath: "../images/oz/wickedwitchofthewest.png"
+  },
+  winged: {
+    level: 6,
+    imagePath: "../images/oz/wingedmonkeys.png"
+  },
+  wolves: {
+    level: 4,
+    imagePath: "../images/oz/wolves.png"
+  }
+};
+let munchkinModifier = document.getElementsByClassName("munchkin-modifier");
+
+// Methods of this class will be extended by Munchkins and Monsters
+class Character {
+  constructor({ level }) {}
+
+  modifier(damage) {
+    this.level += damage;
+    return this.level;
+  }
+}
+
+class Munchkin extends Character {
+  constructor(options) {
+    super(options);
+    this.name = "Munchkin";
+  }
+
+  getLevel() {
+    this.level = parseInt(document.querySelector(".munchkin-level").value);
+    return this.level;
+  }
+}
+
+class Monster extends Character {
+  constructor(options) {
+    super(options);
+    this.imagePath;
+    this.name;
+    this.level;
+  }
+
+  getName() {
+    this.name = document.querySelector(".monster-name").value;
+    return this.name;
+  }
+
+  getImagePath() {
+    this.imagePath = "images/marvel/" + this.name + ".png";
+    return this.imagePath;
+  }
+
+  getMonsterLevel() {
+    this.level = cards[this.name].level;
+    return this.level;
+  }
+}
+
+const munchkin = new Munchkin({});
+
+const monster = new Monster({});
+
+var startButton = document.querySelector(".start-battle");
+
+startButton.onclick = function() {
+  console.log("battle has started");
+  munchkin.getLevel();
+  monster.getName();
+  monster.getImagePath();
+  console.log(monster.name);
+  console.log(monster.imagePath);
+  monster.getMonsterLevel();
+  document.querySelector(".card").style.display = "block";
+};
+
+// let Monster = class {
+//     constructor(name, level, modifier, treasure) {
+//       this.name = name;
+//       this.level = level;
+//       this.modifier = modifier;
+//       this.treasure = treasure;
+
+//       console.log(this.name + " is level " + this.level + ", and has " + this.treasure + " treasures.")
+//     }
+//   };
+
+//   const venom = new Monster("Venom", 10, 0, 2);
+//   const carnage = new Monster("Carnage", 20, 0, 3);
+//   const crows = new Monster("Crows", 1, 0, 1);
