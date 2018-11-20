d6ed29149f4f5dc14e4f0c405a4e3e43d67f8ea3
CodeyPacker <codeypacker@gmail.com>
2018-11-20 15:20:57 -0700

    added browser-sync

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,43 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
 
 gulp.task('styles', function() {
     gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css/'))
+        browserSync.reload();
 });
 
-//Watch task
-gulp.task('default',function() {
+// create a task that ensures the `js` task is complete before
+// reloading browsers
+gulp.task('js-watch', function() {
+    browserSync.reload();
+});
+
+
+// use default task to launch Browsersync and watch JS files
+gulp.task('default', function () {
+
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    // add browserSync.reload to the tasks array to make
+    // all browsers reload after tasks are complete.
+    gulp.watch("js/*.js", ['js-watch']);
     gulp.watch('sass/**/*.scss',['styles']);
 });
