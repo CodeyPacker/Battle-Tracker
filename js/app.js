73d781533d1dd3d2e7f2408d1a3bb095ff396b2a
CodeyPacker <codeypacker@gmail.com>
2018-11-28 14:25:34 -0700

    Added Jennys rick and morty monsters

--- js/app.js
+++ js/app.js
@@ -10,15 +10,216 @@ const jenny = [
     bad: "Pathetic! -5 in your next combat."
   },
   {
-    name: "abomination",
-    set: "marvel",
-    power: 14,
-    treasure: 4,
+    name: "frank palicky",
+    set: "rick and morty",
+    power: 1,
+    treasure: 1,
     levels: 1,
     abilities:
-      "-4 against Hulk. Players in combat with him do not get any combat bonuses from items with a bonus of +3 or less",
-    bad: "Lose two levels and discard two cards from your hand"
+      "+2 against High Schoolers. If there are ice cubes in the play area, he is automatically defeated.",
+    bad: "You know I'm gonna cut you, 'cause my family's rich! Discard 300 Gold Pieces worth of items. If you don't have at least 300, turn in what you have."
+  },
+  {
+    name: "ghost in a jar",
+    set: "rick and morty",
+    power: 1,
+    treasure: 1,
+    levels: 1,
+    abilities:
+      "Your memory betrays you. Without searching the discards, recall the name of the bottommost monster in the Door discards. If you cannot, Ghost in a Jar gets +3.",
+    bad: "Suffer the Bad Stuff of the bottommost monster in the Door discaards"
+  },
+  {
+    name: "centaur",
+    set: "rick and morty",
+    power: 1,
+    treasure: 1,
+    levels: 1,
+    abilities:
+      "Automatically defeated if Scary Terry is your Ally.",
+    bad: "Hangups in the Pleasure Chamber are punishable by death! You are dead."
+  },
+  {
+    name: "photography raptor",
+    set: "rick and morty",
+    power: 1,
+    treasure: 1,
+    levels: 1,
+    abilities:
+      "+2 for each Parasite that adds to his level.",
+    bad: "Flash! Blindly discard one card from your hand."
+  },
+  {
+    name: "tuberculosis",
+    set: "rick and morty",
+    power: 2,
+    treasure: 1,
+    levels: 1,
+    abilities:
+      "Guns do not count against it.",
+    bad: "Discard any Guns you have in play."
+  },
+  {
+    name: "amish cyborg",
+    set: "rick and morty",
+    power: 2,
+    treasure: 1,
+    levels: 1,
+    abilities:
+      "-1 against anyone with a beard. +2 for each electronic device on the table.",
+    bad: "Give a card of your choice to another player."
+  },
+  {
+    name: "uncle steve",
+    set: "rick and morty",
+    power: 2,
+    treasure: 1,
+    levels: 1,
+    abilities:
+      "+3 if you have a trip planned within the next two weeks.",
+    bad: "Discard a One-Shot item. If you cannot, lose a level."
+  },
+  {
+    name: "mrs. refrigerator",
+    set: "rick and morty",
+    power: 2,
+    treasure: 1,
+    levels: 1,
+    abilities:
+      "The jig is up! When she enters combat, reveal your hand and discard any Traps you are holding.",
+    bad: "Suffer the effects of the topmost Trap in the Door discards."
+  },
+  {
+    name: "hamurai",
+    set: "rick and morty",
+    power: 2,
+    treasure: 1,
+    levels: 1,
+    abilities:
+      "Rick, you love those barbeques, Rick.",
+    bad: "Meat your maker. You are dead meat."
+  },
+  {
+    name: "gromflomites",
+    set: "rick and morty",
+    power: 4,
+    treasure: 2,
+    levels: 1,
+    abilities:
+      "Damn bureaucrats. +5 if you've ever had a government related job. -2 if Birdperson is your Ally.",
+    bad: "Cavity search! Discard a random card from your hand."
+  }, 
+  {
+    name: "brad",
+    set: "rick and morty",
+    power: 4,
+    treasure: 2,
+    levels: 1,
+    abilities:
+      "+2 against High Schoolers. +3 if there is anything football related in the room. -3 if you have a college degree.",
+    bad: "Lose any female Allies and any female players may steal a random card from your hand."
+  },
+  {
+    name: "cousin nicky",
+    set: "rick and morty",
+    power: 4,
+    treasure: 2,
+    levels: 1,
+    abilities:
+      "Without searching the dicards, recall the bottommost Trap in the Door discards. If you cannot, Cousin Nicky gets +3.",
+    bad: "Suffer the effects of the bottommost Trap in the Door discards."
+  },
+  {
+    name: "tinkles",
+    set: "rick and morty",
+    power: 4,
+    treasure: 2,
+    levels: 1,
+    abilities:
+      "+3 against Summer.",
+    bad: "Prove you're real. Reveal your hand to other players."
+  },
+  {
+    name: "lucy",
+    set: "rick and morty",
+    power: 4,
+    treasure: 2,
+    levels: 1,
+    abilities:
+      "+3 if anyone is wearing a necklace.",
+    bad: "Forced to re-enact the car scene. Lose two levels or succumb to deth. Your decision."
+  },
+  {
+    name: "general nathan",
+    set: "rick and morty",
+    power: 6,
+    treasure: 2,
+    levels: 1,
+    abilities:
+      "-2 if you have the Particle Beam Wristwatch equipped.",
+    bad: "What America's got is seventy thousand megatons of kaboomboom. Discard three cards."
+  },
+  {
+    name: "zigerions",
+    set: "rick and morty",
+    power: 6,
+    treasure: 2,
+    levels: 1,
+    abilities:
+      "Bare skin! -3 if you are wearing shorts.",
+    bad: "The galaxy's most ambitious and least successful con artists. Roll the die. On a 1 or 2, give an item you have in play to the player on your right."
+  },
+  {
+    name: "poncho",
+    set: "rick and morty",
+    power: 6,
+    treasure: 2,
+    levels: 1,
+    abilities:
+      "+2 for each Gun in play. When he enters combat, reveal your hand: -2 if you have Tuberculosis (the card).",
+    bad: "You catch a disease and die. You are dead."
+  },
+  {
+    name: "evil smith clones",
+    set: "rick and morty",
+    power: 6,
+    treasure: 2,
+    levels: 1,
+    abilities:
+      "From an alternate dimension's future. The player to your left gets -5 in their next combat if the Clones are defeated.",
+    bad: "Demonic alien spirits are really valuable. Discard 500 Gold Pieces worth of items."
+  },
+  {
+    name: "frakenstein",
+    set: "rick and morty",
+    power: 6,
+    treasure: 2,
+    levels: 1,
+    abilities:
+      "You're on the wrong side of the pitchfork. Pick one player to roll the die and add the result to Frankenstein's Level.",
+    bad: "Give a Hand item to a player of your choice."
+  },
+  {
+    name: "king flippy nips",
+    set: "rick and morty",
+    power: 8,
+    treasure: 2,
+    levels: 1,
+    abilities:
+      "Gets a bonus equal to Jerry's Level when he enters combat. He gets nothing if Jerry is not in the game.",
+    bad: "The price of power. Discard 500 Gold Pieces worth of items"
+  },
+  {
+    name: "pencilvester",
+    set: "rick and morty",
+    power: 8,
+    treasure: 2,
+    levels: 1,
+    abilities:
+      "+3 against Rick.",
+    bad: "Discard 2 cards."
   }
+
 ];
 
 const cody = [
@@ -182,7 +383,7 @@ function createMonster() {
   // GETS THE MONSTER NAME FROM THE USERS INPUT
   let monsterName = document.querySelector(".monster-name").value.toLowerCase(),
   // SEARCHES THE MONSTERS OBJECT UNTIL IT FINDS A MATCHING MONSTER
-      newMonster = monsters.find(monster => monster.name === monsterName),
+      newMonster = jenny.find(monster => monster.name === monsterName),
       name = newMonster.name,
       levels = newMonster.levels,
       power = newMonster.power,
